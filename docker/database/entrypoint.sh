446b06b0353351aa386be4768edaf976d2146488
RaduADumitru <radu.dumitru@rocketmail.com>
2022-09-09 17:20:28 +0300

    Query: get words with relation

---
+++ docker/database/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+arangod config "arangod.conf"
+#arangorestore \
+#--input-directory "/dump" \
+#--create-collection true \
+#--create-database true \
+#--overwrite true \
+#--server.username "root" \
+#--server.password "openSesame" \
+#--server.database "dex" 
