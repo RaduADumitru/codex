791fa80417cced991ff4dc92e8ccf35e2c35ce55
RaduADumitru <radu.dumitru@rocketmail.com>
2022-09-13 14:17:38 +0300

    Docker compose import data on startup

--- docker/database/entrypoint.sh
+++ docker/database/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-arangod config "arangod.conf"
-#arangorestore \
-#--input-directory "/dump" \
-#--create-collection true \
-#--create-database true \
-#--overwrite true \
-#--server.username "root" \
-#--server.password "openSesame" \
-#--server.database "dex" 
+apk add ca-certificates wget
+wget --version
+wget --quiet --tries=10 --waitretry=40 http://localhost:8529
+# arangod config "arangod.conf"
+arangorestore \
+--server.endpoint "tcp://localhost:8529"
+--input-directory "/dump" \
+--create-collection true \
+--create-database true \
+--overwrite true \
+--server.username "root" \
+--server.password "openSesame" \
+--server.database "dex" 
