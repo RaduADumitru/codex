cadbd93d5c76cdcb403e168730fad58dbbb1281c
RaduADumitru <radu.dumitru@rocketmail.com>
2022-09-20 17:52:12 +0300

    Remove clutter

--- codex/src/main/java/org/example/codex/CodexConfiguration.java
+++ codex/src/main/java/org/example/codex/CodexConfiguration.java
@@ -3,14 +3,8 @@ package org.example.codex;
 import com.arangodb.ArangoDB;
 import com.arangodb.springframework.annotation.EnableArangoRepositories;
 import com.arangodb.springframework.config.ArangoConfiguration;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@ComponentScan(basePackages = {"org.example.codex.controller"})
 @Configuration
 @EnableArangoRepositories(basePackages = {"org.example.codex"})
 public class CodexConfiguration implements ArangoConfiguration {
